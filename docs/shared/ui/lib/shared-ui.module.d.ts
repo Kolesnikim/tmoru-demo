493b77a5e897796c448d0323d41a81ac6253bedb
Ilia Kolesnik <ilya.m.kolesnik@gmail.com>
2022-12-09 13:37:06 +0200

    dist

---
+++ docs/shared/ui/lib/shared-ui.module.d.ts
@@ -0,0 +1,7 @@
+import * as i0 from "@angular/core";
+import * as i1 from "@angular/common";
+export declare class SharedUiModule {
+    static ɵfac: i0.ɵɵFactoryDeclaration<SharedUiModule, never>;
+    static ɵmod: i0.ɵɵNgModuleDeclaration<SharedUiModule, never, [typeof i1.CommonModule], never>;
+    static ɵinj: i0.ɵɵInjectorDeclaration<SharedUiModule>;
+}
